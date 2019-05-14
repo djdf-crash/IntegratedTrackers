53ea201d0aea4d1c3e3e37293205e7db6e0e3819
djdf-crash <chornobil1986>
2019-05-14 18:22:25 +0300

    add

--- app/src/main/java/com/spybike/integratedtrackers/repo/DataRepository.kt
+++ app/src/main/java/com/spybike/integratedtrackers/repo/DataRepository.kt
@@ -5,21 +5,34 @@ import com.spybike.integratedtrackers.models.FilterModel
 import com.spybike.integratedtrackers.models.PointMarkerModels
 import com.spybike.integratedtrackers.utils.AppConstants
 import org.jsoup.Jsoup
+import java.util.*
 
 class DataRepository {
 
     fun getListDataByFilter(filter: FilterModel, listPointMarkerLiveData: MutableLiveData<List<PointMarkerModels>>) {
+
+        val url: String = "${AppConstants.BASE_URL}/LocationsList?unit_code=${filter.id}" +
+                "&today=${Calendar.getInstance().time}" +
+                "&date=${filter.date}" +
+                "&num_rows=${filter.numberRows}" +
+                "&date_from=${filter.dateFrom}" +
+                "&date_to=${filter.dateTo}" +
+                "&month=${filter.month}" +
+                "&year=${filter.year}" +
+                "&mode=${filter.selectMode?.name}" +
+                "&func=undefined"
+
         var list = listPointMarkerLiveData.value
         if (list == null){
             list = ArrayList<PointMarkerModels>()
         }
 
-        Jsoup.connect(convertObjectToParametrsForUrl(filter))
+        Jsoup.connect(url).get().run {
+            select("tr").forEachIndexed { index, element ->
+                println("$index. $element")
+            }
+        }
 
-    }
-
-    fun convertObjectToParametrsForUrl(filter: FilterModel): String{
-        return AppConstants.LIST_DATA_URL
     }
 
 }
--- app/src/main/java/com/spybike/integratedtrackers/utils/AppConstants.kt
+++ app/src/main/java/com/spybike/integratedtrackers/utils/AppConstants.kt
@@ -18,6 +18,7 @@ object AppConstants {
     const val TABLE_FILTER_LAST_CHANGE: String = "last_change"
 
     const val BASE_URL: String = "http://www.integratedtrackers.com/GPSTrack"
-    const val LOGIN_URL: String = "$BASE_URL/api/login?"
-    const val LIST_DATA_URL: String = "$BASE_URL/LocationsList?"
+    const val LOGIN_URL: String = "$BASE_URL/api/login?user_name=&password"
+    //http://www.integratedtrackers.com/GPSTrack/LocationsList?unit_code=358696048760790&today=2019-05-14&date=2019-05-14&num_rows=1000&date_from=2019-05-14%2000:00&date_to=2019-05-14%2023:59&month=0&year=2013&mode=recent&func=undefined
+
 }
